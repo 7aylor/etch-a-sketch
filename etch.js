14862274feaae8812e79df46a4bac18d929f97f5
Taylor Buchheit <tbuchhei@gmail.com>
2015-03-09 20:36:24 -0700

    final draft with boxes

--- etch.js
+++ etch.js
@@ -1,27 +1,62 @@
+const MAX = 36;
+
 var main = function(){
 
-	var height = 10;
-	var width = 10;
+	drawGrid(16);
 
-	$('<button>').text("New Grid").attr('id', 'newGrid').prependTo('#container').click(function(){
-		height = prompt("What is the height?");
-		width = prompt("What is the width?");
+	colors();
+
+	resizeGrid();
+
+	resetGrid();
+
+};
+
+var colors = function(){
+
+	var bgcolorlist = new Array("#FF0000", "#0040FF", "#00FF00", "#F7FE2E", "#190707", "#BF00FF", "#585858", "#FF8000");
+
+	$('li').hover(function(){
+		var color = bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
+		$(this).css({'background-color':color});
 	});
+};
+
+var drawGrid = function(width){
 
 	for(var i = 1; i <= width; i++)
 	{
 		$('<ul>').addClass('row' + i).appendTo("#container");
-   		for(var j = 1; j <= height; j++)
+   		for(var j = 1; j <= width; j++)
    		{
    			$('<li>').attr('id', 'grid' + j).appendTo('.row' + i);
    		}
 	}
 
-	$('li').hover(function(){
-		$(this).css({'background-color':'blue'});
-	});
+};
 
-	
+var resizeGrid = function(){
+	$('<button>').text("Resize Grid").attr('id', 'resize').prependTo('#container').click(function(){
+		width = prompt("What is the width?");
+		if(width > MAX){
+			alert("Too big! Setting max width to 36");
+			width = MAX;
+		}
+		eraseGrid();
+		drawGrid(width);
+		colors();
+	});
+};
+
+var resetGrid = function(){
+	$('<button>').text("Reset Grid").attr('id', 'reset').prependTo('#container').click(function(){
+		$('li').css({'background-color': 'white'})
+	});
+};
+
+var eraseGrid = function(){
+	$('ul').remove();
+	$('li').remove();
 };
 
 $(document).ready(main);
